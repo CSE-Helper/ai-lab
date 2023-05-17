8b703c09517bacf3213bbabee3ce93f9cb2c6c7e
CSE-Helper <99971263+CSE-Helper@users.noreply.github.com>
2023-05-17 22:37:41 +0530

    Add files via upload

---
+++ Codes/monkeyb.pl
@@ -0,0 +1,26 @@
+move(state(middle,onbox,middle,hasnot),
+   grasp,
+   state(middle,onbox,middle,has)).
+move(state(P,onfloor,P,H),
+   climb,
+   state(P,onbox,P,H)).
+move(state(P1,onfloor,P1,H),
+   drag(P1,P2),
+   state(P2,onfloor,P2,H)).
+move(state(P1,onfloor,B,H),
+   walk(P1,P2),
+   state(P2,onfloor,B,H)).
+canget(state(_,_,_,has)).
+canget(State1) :-
+   move(State1,_,State2),
+   canget(State2).
+
+/*
+First -> Make and Compile 
+?- canget(state(atdoor, onfloor, atwindow, hasnot)).
+true . (put only the dot after true)
+?- trace.
+?- canget(state(atdoor, onfloor, atwindow, hasnot)).
+then keep pressing enter till you see true
+
+*/
